04eea6e52e64601f44158582856295fbb00a5cf6
Ebenezer Isaac <mail@ebenezer-isaac.com>
2024-06-14 00:04:34 +0530

    updating packages

--- index.php
+++ index.php
@@ -405,9 +405,9 @@
         </section>
     </div>
 
-    <script src="js/jquery.min.js"></script>
-    <script defer src="js/bootstrap.bundle.min.js"></script>
-    <script defer src="js/jquery.easing.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha512-ahmSZKApTDNd3gVuqL5TQ3MBTj8tL5p2tYV05Xxzcfu6/ecvt1A0j6tfudSGBVuteSoTRMqMljbfdU0g2eDNUA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script defer src="js/resume.min.js"></script>
     <script>
         function init() {
